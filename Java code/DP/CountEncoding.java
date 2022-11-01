e5ad80a179bcb474f9933e4d711d50a8e33997bc
Vishesh-Kumar <19vishesh@gmail.com>
2022-11-02 03:12:57 +0530

    commit

---
+++ Java code/DP/CountEncoding.java
@@ -0,0 +1,10 @@
+// https://www.pepcoding.com/resources/online-java-foundation/dynamic-programming-and-greedy/count-encodings-official/ojquestion
+
+
+import java.util.Scanner;
+
+public class CountEncoding {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
